647c1213c61897dbea9d1213050978b2c0c8100c
Elyas Abebe <ellayab804@gmail.com>
2023-11-03 11:41:57 +0300

    website modified 03-11-2023

--- src/component/PricingCard.js
+++ src/component/PricingCard.js
@@ -14,9 +14,9 @@ const PricingCard = ({ name, price, features }) => {
     <div
       className={`group pricing-container bg-slate-200  sm:w-[320px] max-w-[370px] min-h-[470px] rounded-xl flex flex-col justify-between py-6 px-4 lg:px-4 md:px-3  ${
         name.toLowerCase() === "pro"
-          ? "scale-105 hover:scale-[1.06]"
-          : "lg:hover:scale-[1.005]"
-      }  hover:-translate-y-2 hover:shadow-xl duration-300 my-4 lg:my-0 mx-1 `}
+          ? "scale-105 hover:scale-[1.06] shadow-xl"
+          : "lg:hover:scale-[1.005] hover:shadow-xl"
+      }  hover:-translate-y-2  duration-300 my-4 lg:my-0 mx-1 `}
     >
       <div className=" mt-3">
         <h2 className=" text-center text-2xl font-semibold duration-200 ">
@@ -41,9 +41,9 @@ const PricingCard = ({ name, price, features }) => {
       <button
         className={`${
           name.toLowerCase() === "pro"
-            ? "bg-secondaryColor group-hover:bg-primaryColor"
-            : "bg-primaryColor"
-        } font-semibold text-white group-hover:text-secondaryColor py-3 rounded-full w-3/4 max-w-40 mx-auto duration-500 `}
+            ? "bg-secondaryColor group-hover:text-primaryColor"
+            : "bg-primaryColor group-hover:text-secondaryColor"
+        }  text-white font-semibold  py-3 rounded-full w-3/4 max-w-40 mx-auto duration-500 `}
       >
         Start Now!
       </button>
